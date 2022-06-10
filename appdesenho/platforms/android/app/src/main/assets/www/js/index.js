e30a2ae4c3cf819ed3c17f8693fddf55e003d62d
Taynara <taynaraax0@hotmail.com>
2022-06-09 21:14:15 -0300

    subi

--- appdesenho/platforms/android/app/src/main/assets/www/js/index.js
+++ appdesenho/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,33 +1,29 @@
-let quadro = document.getElementById("quadro")
-let lousa = quadro.getContext("2d")
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-quadro.width = window.innewidth 
-quadro.innerHeight = window.innerHeight
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
 
-lousa.fillStyle = "#eee"
-lousa.fillRect(0,0,quadro.width,quadro.height)
-
-lousa.lineWidth = 5;
-quadro.addEventListener("touchstart",function(evento){
-    lousa.beginPath()
-    lousa.moveto(
-        evento.touches[0].pageX,
-        evento.touches[0].pageY
-        
-    )
-    evento.preventDefault()
-})
-
-quadro.addEventListener('touchmove', function(evento) {
-    lousa.lineTo(
-        evento.touches[0].pageX,
-        evento.touches[0].pageY
-    )
-
-    lousa.strokeStyle = "#000"
-    lousa.lineWidth = 5
-    lousa.stoke()
-    evento.preventDefault()
-    
-});
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
